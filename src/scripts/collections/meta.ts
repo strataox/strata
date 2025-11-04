68c949c397d1103d1c5a4b3f6b96ccfbe6b2a419
Kabolobari Benakole <kb@k16e.com>
2025-11-04 12:21:59 +0100

    added sola

--- src/scripts/collections/meta.ts
+++ src/scripts/collections/meta.ts
@@ -2,14 +2,23 @@
 
 import { z, defineCollection } from 'astro:content'
 import { glob } from 'astro/loaders'
-import { _linkSchema } from '@scripts/utils/link-schema'
 
 export const meta = defineCollection({
-    loader: glob({ pattern: 'meta.{yaml,yml,json}', base: './content/meta' }),
-    schema: ({ image }) => z.object({
-        site: z.object({
-            title: z.string(),
-            description: z.string(),
-        }),
-    }),
+	loader: glob({ pattern: 'meta.{yaml,yml,json}', base: './content/meta' }),
+	schema: ({ image }) =>
+		z.object({
+			site: z.object({
+				title: z.string(),
+				description: z.string(),
+				url: z.string().url(),
+				logo: z.object({
+					src: z.string(),
+					alt: z.string(),
+				}),
+				pattern: z.object({
+					src: z.string(),
+					alt: z.string(),
+				}),
+			}),
+		}),
 })
