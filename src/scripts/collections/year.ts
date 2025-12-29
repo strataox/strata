31537f03909384e52f26fa4a9660f48edcf9cd8c
kb <kb@k16e.com>
2025-12-30 00:09:54 +0100

    ...

--- src/scripts/collections/year.ts
+++ src/scripts/collections/year.ts
@@ -14,6 +14,24 @@ export const year = defineCollection({
 			description: z
 				.string()
 				.max(160, 'Description must not exceed 160 characters'),
+			video: z
+				.object({
+					title: z
+						.string()
+						.max(40, 'Video title must not exceed 40 characters'),
+					description: z
+						.string()
+						.max(
+							155,
+							'Video description must not exceed 155 characters',
+						),
+					cta: z
+						.string()
+						.max(15, 'Video CTA must not exceed 15 characters'),
+					clip: z.string(),
+					video: z.string(),
+				})
+				.optional(),
 			draft: z.boolean().default(false),
 		}),
 })
